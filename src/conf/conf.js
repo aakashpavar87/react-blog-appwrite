0cd63cb64829a8533c8041be42dfe776de044aba
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-20 20:23:19 +0530

    modified env for tiny mce

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteDB: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteBucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    tinyMceKey: String(import.meta.env.VITE_TINY_MCE_API_KEY)
+    tinyMceKey: String(import.meta.env.VITE_VERCEL_TINY_MCE_API_KEY)
 }
 
 export default conf;
